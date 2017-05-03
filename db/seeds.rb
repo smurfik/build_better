31319943017b5318b419e6872c6f03cc2eb79ae8
smurfik <tammy@mlpinit.com>
2017-05-03 21:59:10 +0300

    Generate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+company = Company.create(name: "Sunny Construction")
+company.create_address(street: "4 Sunny Street", city: "Seattle", state: "WA", zip: "98102")
+
+admin = User.create(
+          username: "admin",
+          email: "admin@example.com",
+          company_id: company.id,
+          password: "go",
+          password_confirmation: "go",
+          admin: true)
+admin.create_address(street: "The Street", city: "Phoenix", state: "AZ", zip: "85001")
+
+project_1 = Project.create(
+  name: "The Tallest Building",
+  description: "Prior to 1998, the tallest building status was determined by the height of the building to the top of its architectural elements including spires, but not including 'temporary' structures (such as antennas or flagpoles), which could be added or changed relatively easily without requiring major changes to the building's design.",
+  image_url: "https://placekitten.com/200/201",
+  company_id: company.id)
+
+project_1.create_address(street: "123 Main Street", city: "Rockville", state: "MD", zip: "20853")
+
+project_2 = Project.create(
+  name: "The Pool",
+  description: "Prior to 1998, the tallest building status was determined by the height of the building to the top of its architectural elements including spires, but not including 'temporary' structures (such as antennas or flagpoles), which could be added or changed relatively easily without requiring major changes to the building's design.",
+  image_url: "https://placekitten.com/200/201",
+  company_id: company.id)
+
+project_2.create_address(street: "123 East Street", city: "Seattle", state: "WA", zip: "98102")
