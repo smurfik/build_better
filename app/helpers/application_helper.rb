6787babb517921f4f78db1e660f1e748a195af04
smurfik <tammy@mlpinit.com>
2017-05-04 22:55:53 +0300

    Add gravatar for user's photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def gravatar_for(email)
+    hashed_email = Digest::MD5.hexdigest(email.downcase.strip)
+    image_tag "http://www.gravatar.com/avatar/#{hashed_email}?s=50.jpg", alt: "Profile Photo"
+  end
 end
